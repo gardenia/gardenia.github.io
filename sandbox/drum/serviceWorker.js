04ca82e367a3b523f55712e7077596da6d949cdf
colm <colm>
2020-04-12 15:34:19 +0100

    add serviceWorker.js

--- sandbox/drum/serviceWorker.js
+++ sandbox/drum/serviceWorker.js
@@ -1,24 +1,32 @@
 const staticAssets = [
-	'/sandbox/drum/index.html',
-	'/sandbox/drum/dist/img/favicon.ico',
-	'/sandbox/drum/dist/css/app.css',
-	'/sandbox/drum/dist/js/index.js',
-	'/sandbox/drum/dist/js/Tone.js',
+	'./index.html',
+	'./dist/img/favicon.ico',
+	'./dist/css/app.css',
+	'./dist/js/index.js',
+	'./dist/js/Tone.js',
+	'./dist/js/Tone.js.map',
 ];
 
 self.addEventListener('install', async () => {
+    console.log("serverWorker install fired");
 	const cache = await caches.open('static-cache');
+    console.log("serverWorker pre cache.addAll", staticAssets);
 	cache.addAll(staticAssets);
+    console.log("serverWorker post cache.addAll", staticAssets);
 });
 
 self.addEventListener('fetch', event => {
+    console.log("serverWorker fetch fired", req.url);
 	const req = event.request;
-	const url = new URL(req.url);
     console.log("fetching", req.url);
+	const url = new URL(req.url);
+    console.log("comparing url.origin=", url.origin, " with location.url=", location.url);
 
 	if (url.origin === location.url) {
+        console.log("fetching from cache", req);
 		event.respondWith(cacheFirst(req));
 	} else {
+        console.log("NOT fetching from cache", req);
 		event.respondWith(networkFirst(req));
 	}
 });
