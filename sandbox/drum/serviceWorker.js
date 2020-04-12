7c424213276257f4992fec9e708ceae7a877a28a
colm <colm>
2020-04-12 14:54:41 +0100

    add serviceWorker.js

---
+++ sandbox/drum/serviceWorker.js
@@ -0,0 +1,39 @@
+const staticAssets = [
+	'./index.html',
+	'./dist/img/favicon.ico',
+	'./dist/css/app.css',
+	'./dist/js/index.js',
+];
+
+self.addEventListener('install', async () => {
+	const cache = await caches.open('static-cache');
+	cache.addAll(staticAssets);
+});
+
+self.addEventListener('fetch', event => {
+	const req = event.request;
+	const url = new URL(req.url);
+
+	if (url.origin === location.url) {
+		event.respondWith(cacheFirst(req));
+	} else {
+		event.respondWith(networkFirst(req));
+	}
+});
+
+async function cacheFirst (req) {
+	const cachedResponse = caches.match(req);
+	return cachedResponse || fetch(req);
+}
+
+async function networkFirst (req) {
+	const cache = await caches.open('dynamic-cache');
+
+	try {
+		const res = await fetch(req);
+		cache.put(req, res.clone());
+		return res;
+	} catch (error) {
+		return await cache.match(req);
+	}
+}
